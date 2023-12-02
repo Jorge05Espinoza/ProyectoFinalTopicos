6a3b3e49bb9c19e3d2e431e66731e8c3be0dd0d8
Jorge05Espinoza <benito05camela@gmail.com>
2023-12-01 20:11:03 -0700

    Se agrego un boton con el cual se mostaran los juegos que son juardados y en script se agregaron funciones en la que sirve para mostrar los juegos guardados y se agrego la funcion donde se agrego una donde sirve para guardar los juegos

--- script.js
+++ script.js
@@ -1,58 +1,109 @@
-const apiKey = 'dd5f2a8446fd4105a5a4bf528ff33999';  // Reemplaza con tu clave de API de RAWG
+const apiKey = 'dd5f2a8446fd4105a5a4bf528ff33999';
 const videoGameList = document.getElementById('videoGame-container');
-const variableVacia=0;
+let carritoGuardar = [];
+let tipoVideogame = [];
 
 document.getElementById('search').addEventListener('click', () => {
   displayVideoGame();
 });
 
 document.getElementById('carroGuardar').addEventListener('click', () => {
-    displayVideoGame();
-  });
+  buttoncarritoGuardar();
+});
 
+document.getElementById('mostrarGuardados').addEventListener('click', () => {
+    mostrarJuegosGuardados();
+});
 
-const gameId =3790; 
 async function displayVideoGame() {
+  videoGameList.innerHTML = ''; // Limpiar el contenido actual
+
   const gameName = document.getElementById('nameGame').value;
   const videoGame = await getVideoGame(gameName);
-  if(!gameName){
-      const inputError=document.createElement('p');
-      inputError.innerHTML='<string>Por favor, ingrese el nombre de un Pokémon</string>';
-      videoGameList.appendChild(inputError);  
-  }else if (videoGame) {
-      addVideoGame(videoGame);
+
+  if (!gameName) {
+    const inputError = document.createElement('p');
+    inputError.innerHTML = '<string>Por favor, ingrese el nombre de un Pokémon</string>';
+    videoGameList.appendChild(inputError);
+  } else if (videoGame) {
+    addVideoGame(videoGame);
   } else {
-      const Errormessage = document.createElement('p');
-      Errormessage.innerHTML = '<string>Lo Sentimos, no se ha encontrado el Pokémon deseado</string>';
-      videoGameList.appendChild(Errormessage);
+    const errorMessage = document.createElement('p');
+    errorMessage.innerHTML = '<string>Lo Sentimos, no se ha encontrado el Pokémon deseado</string>';
+    videoGameList.appendChild(errorMessage);
   }
 }
 
 async function getVideoGame(name) {
   try {
-      let response = await fetch(`https://api.rawg.io/api/games/${name}?key=${apiKey}`);
-      if (response.ok) {
-          return await response.json();
-      } else {
-          throw new Error(`Error de red: ${response.status}`);
-      }
+    let response = await fetch(`https://api.rawg.io/api/games/${name}?key=${apiKey}`);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(`Error de red: ${response.status}`);
+    }
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
 }
 
 function addVideoGame(videoGame) {
   const element = document.createElement('div');
-  const genresList = videoGame.genres.map(genre => genre.name);
+  const genresList = videoGame.genres.map((genre) => genre.name);
   element.innerHTML = `
-      <img src="${videoGame.background_image}" alt="${videoGame.name}">
-      <strong>Id</strong>: ${videoGame.id}
-      <strong>Name</strong>: ${videoGame.name}
-      <strong>Description</strong>: ${videoGame.description}
-      <strong>Fecha de lanzamiento</strong>: ${videoGame.released}
-      <strong>Genero</strong>: ${genresList.join(', ')}
+    <img src="${videoGame.background_image}" alt="${videoGame.name}">
+    <strong>Id</strong>: ${videoGame.id}
+    <strong>Name</strong>: ${videoGame.name}
+    <strong>Description</strong>: ${videoGame.description}
+    <strong>Fecha de lanzamiento</strong>: ${videoGame.released}
+    <strong>Genero</strong>: ${genresList.join(', ')}
   `;
-      videoGameList.appendChild(element); 
+  videoGameList.appendChild(element);
 }
+async function buttoncarritoGuardar() {
+    const gameName = document.getElementById('nameGame').value;
+    if (gameName) {
+      const newVideoGame = await getVideoGame(gameName);
+      if (newVideoGame) {
+        const isAlreadyInCart = carritoGuardar.some((game) => game && game.id === newVideoGame.id);
+        if (!isAlreadyInCart) {
+          carritoGuardar.push(newVideoGame);
+          console.log('Juego agregado al carrito:', newVideoGame.name);
+        } else {
+          console.log('El juego ya está en el carrito:', newVideoGame.name);
+        }
+      } else {
+        console.log('No se encontró información para el juego:', gameName);
+      }
+    }
+  }
 
-
+function mostrarJuegosGuardados() {
+    videoGameList.innerHTML = ''; // Limpiar el contenido actual
+  
+    if (carritoGuardar.length === 0) {
+      const carritoVacio = document.createElement('h1');
+      carritoVacio.innerHTML = '<string>No has guardado juegos</string>';
+      videoGameList.appendChild(carritoVacio);
+      return;
+    }
+  
+    const carritoHTML = carritoGuardar.map((videoGame) => {
+      if (videoGame && videoGame.genres) {
+        const genresList = videoGame.genres.map((genre) => genre.name);
+  
+        return `
+          <div>
+            <img src="${videoGame.background_image}" alt="${videoGame.name}">
+            <strong>Id</strong>: ${videoGame.id}
+            <strong>Name</strong>: ${videoGame.name}
+            <strong>Description</strong>: ${videoGame.description}
+            <strong>Fecha de lanzamiento</strong>: ${videoGame.released}
+            <strong>Genero</strong>: ${genresList.join(', ')}
+          </div>
+        `;
+      }
+    }).join('');
+  
+    videoGameList.innerHTML = carritoHTML;
+}
